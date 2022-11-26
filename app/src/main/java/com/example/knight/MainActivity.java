c6943d0fba0a6cbd07948503962861c9d2bdfcad
Yechiel Shirion <100494204+yshirion@users.noreply.github.com>
2022-11-26 22:55:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/knight/MainActivity.java
@@ -0,0 +1,166 @@
+package com.example.knight;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    final short SIZE = 8; //For change the size of board (for not 'real' chess board).
+    double relative = SIZE/8;
+    double x = 1/relative;
+    GridLayout chess;
+    Button[][] arr;
+    Button refresh;
+    boolean isDest = false; //Realize which press now - first for start either the end.
+    boolean isEven = true; //For paint the board - like chess board.
+    TextView tv;
+    String text;
+    int[] start;
+    int[] end;
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialize();
+        initializeAgain();
+        buildGrid();
+    }
+    //Initialize all screen components.
+    private void initialize() {
+        chess = findViewById(R.id.chessBoard);
+        chess.setColumnCount(SIZE);
+        chess.setRowCount(SIZE);
+        tv = findViewById(R.id.instruction);
+        text = "";
+        tv.setText("Please press on your desire start");
+        tv.setTextColor(Color.GRAY);
+        arr = new Button[SIZE][SIZE];
+    }
+    //Define the 'again' button' to reset the board for other game.
+    private void initializeAgain() {
+        refresh = findViewById(R.id.button);
+        refresh.setOnClickListener(v -> {
+            finish();
+            overridePendingTransition(0, 0);
+            startActivity(getIntent());
+            overridePendingTransition(0, 0);
+        });
+    }
+
+    //Create buttons and push them like a part of the chess board.
+    private void buildGrid() {
+        for (int i=0;i< arr.length;i++) {
+            for (int j=arr[i].length-1;j>=0;j--) {
+                arr[i][j] = new Button(this);
+                chess.addView(arr[i][j]);
+                setButton(arr[i][j]);
+                arr[i][j].setText(""+(i)+","+(j));
+                arr[i][j].setTextSize((int) (10*x));
+                arr[i][j].setTextColor(Color.WHITE);
+                arr[i][j].setTag(new int[]{i,j});//Data in button for connection of button and its position in array.
+            }
+            isEven = !isEven;
+        }
+    }
+
+    //For each square in chess board.
+    private void setButton(Button b) {
+
+        setParam(b);
+        paintButton(b);
+        b.setOnClickListener(this);
+    }
+
+    private void paintButton(Button b) {
+        if (isDest)
+            b.setBackgroundColor(Color.BLUE);
+        else {
+            //For board uses the background for change the default background of every button.
+            if (isEven) b.setBackgroundResource(R.drawable.white);
+            else b.setBackgroundResource(R.drawable.black);
+            isEven = !isEven;
+        }
+    }
+
+    //UI of chess board.
+    private void setParam(Button b) {
+        GridLayout.LayoutParams param = (GridLayout.LayoutParams) b.getLayoutParams();
+        param.height = (int) (120*x);
+        param.width = (int) (120*x);
+        param.rightMargin = 3;
+        param.leftMargin = 3;
+        param.bottomMargin = 3;
+        param.topMargin = 3;
+        b.setLayoutParams(param);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button b = (Button) v;
+        if (!isDest) {
+            startClick(b);
+        }
+        else{
+            endClick(b);
+        }
+    }
+
+    //First click - define the start position.
+    private void startClick(Button b) {
+        int[] position = (int[]) b.getTag();
+        isDest = true;
+        start = new int[]{position[0], position[1]};
+        b.setBackgroundColor(Color.BLUE);
+        b.setText("start");
+        b.setEnabled(false);
+        setHead(true);
+    }
+
+    //Second click - define the destination, lock the board to more clicks, and start the calculation.
+    private void endClick(Button b) {
+        int[] position = (int[]) b.getTag();
+        b.setBackgroundColor(Color.RED);
+        end = new int[]{position[0], position[1]};
+        b.setText("end");
+        for (int i=0;i< arr.length;i++) for (int j=arr[i].length-1;j>=0;j--)
+            arr[i][j].setEnabled(false);
+        setHead(false);
+
+        //Send the problem to solve by Solution object.
+        Solution solution = new Solution( SIZE, start,end);
+        Solution.Move move = solution.play();
+        paintPath(move);
+    }
+
+    //Text box for instruction and describe the situation.
+    private void setHead(boolean isStart){
+        if (isStart){
+            tv.setText("Now choose your destination.");
+            text += "your start is :(" + (start[0])+","+(start[1])+"),\n";
+        }
+        else {
+            text += "and your destination is :(" + (end[0])+","+(end[1])+").";
+            tv.setText(text);
+        }
+    }
+
+    //Paint the path after the algorithm finished its calculate.
+    private void paintPath(Solution.Move path) {
+        if (path == null) return;
+        while (path.parent != null){
+            arr[path.i][path.j].setBackgroundColor(Color.YELLOW);
+            arr[path.i][path.j].setText(""+path.num);
+            arr[path.i][path.j].setTextColor(Color.BLACK);
+            path = path.parent;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/knight/Solution.java
@@ -0,0 +1,99 @@
+package com.example.knight;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+//Solution by BFS method.
+public class Solution extends AppCompatActivity{
+
+    final short MIN = 0;
+    final short MAX; // for different size of board.
+    boolean[][] map; // mapping of the board to mark the visited nodes, to avoid try them again.
+    Move tmp = new Move();
+
+    //This class hold the data for each 'node'.
+    public class Move implements Comparable<Move>{
+
+        int i;
+        int j;
+        int num;
+        Move parent;
+        private Move(){}
+        //For the first node.
+        private Move(int i, int j){
+            this.i = i;
+            this.j = j;
+            num = 0;
+        }
+        //Define the node as 'son' of the its previous node.
+        private Move(int i, int j, Move parent){
+            this.i = i;
+            this.j = j;
+            this.parent = parent;
+            num = parent.num+1; //Its number on the path from the start node.
+        }
+
+        //Compare method for priorityQueue.
+        @Override
+        public int compareTo(Move o) {
+
+            if ((o.i - end.i) +(o.j - end.j) > (this.i - end.i) +(this.j - end.j)) return -1;
+            return 1;
+        }
+    }
+
+    Move start;
+    Move end;
+    ArrayList<Move> explore;
+
+    //Define start and end node, and the data structure for the calc.
+    public Solution(short max, int[] start, int[] end){
+
+        MAX = (short) (max-1);
+        this.start = new Move(start[0],start[1]);
+        this.end = new Move(end[0],end[1]);
+        explore =  new ArrayList<>();
+        explore.add(this.start);
+        map = new boolean[MAX+1][MAX+1];
+    }
+
+    //The method that been invoked by the mainActivity.
+    public Move play(){
+
+        while (!explore.isEmpty()){
+            tmp = explore.get(0);
+            explore.remove(0);
+            if(search()) break;
+        }
+
+        return tmp;
+    }
+
+    //BFS search, to find the fastest way, that for sure find the fastest, because this the property of BFS search.
+    private boolean search(){
+        if (map[tmp.i][tmp.j]) return false; //This node is already watched.
+        map[tmp.i][tmp.j] = true; //mark this position in map that watched.
+
+        //Try to improve the performance by order the nodes by the close to end node by use PriorityQueue.
+        PriorityQueue<Move> tmpList = new PriorityQueue<>();
+        // All sons of this node.
+        tmpList.add(new Move(tmp.i-2,tmp.j+1,tmp));
+        tmpList.add(new Move(tmp.i-2,tmp.j-1,tmp));
+        tmpList.add(new Move(tmp.i+2,tmp.j+1,tmp));
+        tmpList.add(new Move(tmp.i+2,tmp.j-1,tmp));
+        tmpList.add(new Move(tmp.i+1,tmp.j+2,tmp));
+        tmpList.add(new Move(tmp.i+1,tmp.j-2,tmp));
+        tmpList.add(new Move(tmp.i-1,tmp.j+2,tmp));
+        tmpList.add(new Move(tmp.i-1,tmp.j-2,tmp));
+
+        while (!tmpList.isEmpty()){
+            Move a = tmpList.poll();
+            if (a.i > MAX || a.i < MIN || a.j > MAX || a.j < MIN) continue; //out of the range - not enter to explore list.
+            if (a.i == end.i && a.j == end.j) return true; //This node is the target node - check before enter it to 'explore' - BFS attribute.
+            explore.add(a);
+         }
+
+        return false;
+    }
+}
